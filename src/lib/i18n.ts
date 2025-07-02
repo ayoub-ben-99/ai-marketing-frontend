3b633e5947a5118c29c7cb46084ac92d81a27bdc
AYOUB / <ayoubbenrkia78@gmail.com>
2025-07-02 17:05:45 +0100

    update files

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -1,9 +1,9 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 
-import ar from './locales/ar.json';
-import en from './locales/en.json';
+import ar from "./locales/ar.json";
+import en from "./locales/en.json";
 
 i18n
   .use(LanguageDetector)
@@ -13,7 +13,8 @@ i18n
       ar: { translation: ar },
       en: { translation: en },
     },
-    fallbackLng: 'ar',
+    fallbackLng: "ar",
+    debug: false,
     interpolation: { escapeValue: false },
   });
 
